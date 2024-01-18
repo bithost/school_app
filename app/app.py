da1e08e5ef4fa19522f52401b1755f69b245ad0b
Tadas <tadasa@gmail.com>
2024-01-18 13:25:43 +0000

    build updated app

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, This is Dockerized Flask App Behind Load Balancer!'
+    return 'This is Dockerized Flask App Behind Load Balancer!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
